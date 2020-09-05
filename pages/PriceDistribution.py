c3bd7e043262b19c7cf4de1301bd0dc00cafb082
Hemantkumar <hemantharijan92@gmail.com>
2020-09-05 20:06:08 +0530

    Year to Year Price distribution Done

--- pages/PriceDistribution.py
+++ pages/PriceDistribution.py
@@ -9,6 +9,8 @@ import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
 
+from scipy.stats import pearsonr
+
 
 df = pd.DataFrame()
 def carData(data):
@@ -36,8 +38,25 @@ def scatterPlot():
     )))
     return st.plotly_chart(fig)
 
+corr = 0.0
+def Corr():
+    global corr
+    year = df_brand['Year_Of_Registration']
+    price = df_brand['Price_inEURO']    
+    corr,_ = pearsonr(year, price)              #Finding Correlation
+    return corr
+
 
 def write():
+
+    components.html(f"""
+             <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+                <div class="flex">
+                    <div class="flex-auto rounded-md shadow-lg overflow-hidden text-white font-bold rounded-md text-xl bg-blue-500 text-center px-4 py-4 m-2">
+                    Price Distribution
+                    </div>
+                </div>
+    """,height=75)
 
     data = st.sidebar.file_uploader('upload car data file', type='csv')
     
@@ -50,47 +69,77 @@ def write():
 
         data_count = df_brand.shape
         ywmd = df_brand.loc[df_brand['Price_inEURO']==df_brand['Price_inEURO'].max()]
-        ywmd = ywmd['Year_Of_Registration'].values.tolist()
+        #print(ywmd)
+        ywmd = ywmd.values.tolist()
 
-        components.html(f"""
+        Corr()
+        Menu1 = ["Year to Year","Month wise","Average distribution per year"]
+            
+        choice = st.sidebar.radio("",list(Menu1))
+
+        if choice == "Year to Year":
+            #print(choice)
+            components.html(f"""
             <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-
-            <div class="flex">
+            <div class="flex rounded-lg shadow-lg overflow-hidden rounded-md bg-blue-500 px-4 py-4">
+            <div class="flex flex-wrap gap-x-2  gap-y-2">
     
-                <div class="max-w-sm rounded-md overflow-hidden shadow-lg">
+                <div class="rounded-md bg-white overflow-hidden shadow-lg">
                     <div class="px-6 pt-2">
-                        <div class="text-l"><span>Total Count</span></div>
+                        <div class="text-l text-center"><span>Total Count</span></div>
                     </div>
                     <div class="px-6 font-bold text-xl pb-2"><span>{data_count[0]}</span></div>
                 </div>
     
-                <div class="max-w-sm rounded-md overflow-hidden shadow-lg">
+                <div class="rounded-md bg-white overflow-hidden shadow-lg">
                     <div class="px-6 pt-2">
-                        <div class="text-l"><span>Max Price</span></div>
+                        <div class="text-l text-center"><span>Price Range</span></div>
                     </div>
-                    <div class="px-6 font-bold text-xl pb-2"><span>{df_brand['Price_inEURO'].max()} €</span></div>
-                </div>
-    
-                <div class="max-w-sm rounded-md overflow-hidden shadow-lg">
-                    <div class="px-6 pt-2">
-                        <div class="text-l"><span>Min Price</span></div>
-                    </div>
-                    <div class="px-6 text-xl font-bold pb-2"><span>{df_brand['Price_inEURO'].min()} €</span></div>
+                    <div class="px-6 font-bold text-xl pb-2"><span>{df_brand['Price_inEURO'].min()} € - {df_brand['Price_inEURO'].max()} €</span></div>
                 </div>
 
-                <div class="max-w-sm rounded-md overflow-hidden shadow-lg">
+                <div class="rounded-md bg-white overflow-hidden shadow-lg">
                     <div class="px-6 pt-2">
-                        <div class="text-l"><span>Year with max distribution</span></div>
+                        <div class="text-l text-center"><span>Correlation</span></div>
                     </div>
-                    <div class="px-6 pb-2 text-xl font-bold"><span>{ywmd[0]}</span></div>
+                    <div class="px-6 font-bold text-xl pb-2"><span>{"{:.3f}".format(corr)}</span></div>
                 </div>
-            
-            </div>""",height=100)
+
+                 <div class="overflow-hidden px-6 pt-2">
+                    <div >
+                        <div class="text-l text-white"><span>Year with max Distribution</span></div>
+                    </div>
+                    <div class="font-bold text-white text-xl pb-2"><span>{ywmd[0][2]}</span></div>
+                </div>
+
+                <div class="overflow-hidden">
+                    <div class="px-2 pt-2">
+                        <div class="text-l text-white"><span>Model name</span></div>
+                    </div>
+                    <div class="px-2 font-bold text-white text-xl pb-2"><span>{ywmd[0][1]}</span></div>
+                </div>
+
+                <div class="overflow-hidden">
+                    <div class="px-2 pt-2">
+                        <div class="text-l text-white"><span>Km driven</span></div>
+                    </div>
+                    <div class="px-2 font-bold text-white text-xl pb-2"><span>{ywmd[0][4]}</span></div>
+                </div>
+                <div class="overflow-hidden">
+                    <div class="px-2 pt-2">
+                        <div class="text-l text-white"><span>Current Selling price</span></div>
+                    </div>
+                    <div class="px-2 font-bold text-white text-xl pb-2"><span>{ywmd[0][5]}</span></div>
+                </div>
+            </div>
+            </div>""",height=180)
+            scatterPlot()
+
+        elif choice == "Month wise":
+            print(choice)
+        elif choice == "Average distribution per year":
+            print(choice)
         
-        scatterPlot()
-
-        if st.sidebar.checkbox('Show data'):
-            st.write(df_brand)
         
 
     else:
@@ -99,3 +148,7 @@ def write():
 
 if __name__=="__main__":
     write()
+
+
+
+    
